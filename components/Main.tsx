8360b2808e5c2d42b7e802092a86a8edc3cb98bb
manishpropel <93863299+manishpropel@users.noreply.github.com>
2022-10-11 22:38:34 +0530

    minor fix

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import AppIntro from './AppIntro';
-import DzapTrustFactor from './DzapTrustFactor';
-import DzapWorkingModel from './DzapWorkingModel';
 import AppLayout from './layout/AppLayout';
-import MultiToMulti from './MultiToMulti';
 
 const Main = () => {
     return (
-        <AppLayout>
-            <>
-                <AppIntro />
-                <MultiToMulti />
-                <DzapWorkingModel />
-                <DzapTrustFactor />
-            </>
-        </AppLayout>
+        <AppLayout />
     )
 }
 export default React.memo(Main);
